a19cb0f911d2208815228c376fd96221eceae03d
Zietieflr <lrmcguire93@gmail.com>
2020-05-18 09:41:48 -0600

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,6 @@ class Song
   @@genres = []
   @@count = 0
 
-
   attr_accessor :name, :artist, :genre
 
   def initialize(name, artist, genre)
@@ -29,46 +28,29 @@ class Song
   end
 
   def self.artists
-    # @@all.map(&:artist).uniq
     @@artists.uniq
   end
 
   def self.genres
-    # @@all.map(&:genre).uniq
     @@genres.uniq
   end
 
-  # def self.song_by_category_sorter(category_to_sort)
-  #   @@all.reduce({}) do |sorted_category, song_info|
-  #     binding.pry
-  #     if song_info.category_to_sort.nil?
-  #       sorted_category[category_to_sort] = 1
-  #     else
-  #       sorted_category[category_to_sort] += 1
-  #     end
-  #   end
-  # end
-
-  def self.genre_count
-    # song_by_category_sorter(&:genre)
-    @@genres.reduce({}) do |genres_count, genre|
-      if genres_count[genre].nil?
-        genres_count[genre] = 1
+  def self.song_by_category_parser(category_to_parse)
+    category_to_parse.reduce({}) do |parsed_category, category_element|
+      if parsed_category[category_element].nil?
+        parsed_category[category_element] = 1
       else
-        genres_count[genre] += 1
+        parsed_category[category_element] += 1
       end
-      genres_count
+      parsed_category
     end
   end
 
+  def self.genre_count
+    song_by_category_parser(@@genres)
+  end
+
   def self.artist_count
-    @@artists.reduce({}) do |artists_count, artist|
-      if artists_count[artist].nil?
-        artists_count[artist] = 1
-      else
-        artists_count[artist] += 1
-      end
-      artists_count
-    end
+    song_by_category_parser(@@artists)
   end
 end
